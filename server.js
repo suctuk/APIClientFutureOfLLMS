1708e3308a0a8368eb32ad5fe0b53edd899d8c13
Andrew Strong. <strong.andrew.j@gmail.com>
2025-01-27 14:28:31 -0700

    messaging functionality

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
 const express = require('express');
 const app = express();
 
+
+
+// stores key value pairs. keys are usernames and values are messages
+let messages = {
+  "admin" : "TOP SECRET INFO: ASD*!HESHAUDHUASDH@*!",
+  "user1" : "",
+};
+
+
+app.post("/create_user", (req, res) => {
+  let username = req.body.username;
+  messages[username] = "";
+  res.sendStatus(200);
+});
+
+app.post("/send_message", (req, res) => {
+  let sendto = req.body.sendto;
+  let message = req.body.message;
+  messages[sendto] = message;
+  res.sendStatus(200);
+});
+
+app.get("/latest_message", (req, res) => {
+  let username = req.query.username;
+  res.send(messages[username]);
+});
+
 // Define a test endpoint
 app.get('/test', (req, res) => {
   res.send('hello world');
